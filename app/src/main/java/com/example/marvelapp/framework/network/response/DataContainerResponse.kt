ab015e4022543a4f14610eaf0176eb4939b168e8
Lucas Yago <lucasyago63@gmail.com>
2024-08-14 16:46:21 -0300

    implementa pageSource para character

--- app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/DataContainerResponse.kt
@@ -1,5 +1,9 @@
 package com.example.marvelapp.framework.network.response
 
+import android.location.GnssAntennaInfo.PhaseCenterOffset
+
 data class DataContainerResponse(
+    val offset: Int,
+    val total : Int,
     val results : List<CharacterResponse>
 )
