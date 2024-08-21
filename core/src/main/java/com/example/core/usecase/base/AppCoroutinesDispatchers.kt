c01bf5884f4861fe544ea691cc4410ad6e2c96c8
Lucas Yago <lucasyago63@gmail.com>
2024-08-21 11:48:05 -0300

    implementa base usecase

---
+++ core/src/main/java/com/example/core/usecase/base/AppCoroutinesDispatchers.kt
@@ -0,0 +1,10 @@
+package com.example.core.usecase.base
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+data class AppCoroutinesDispatchers(
+
+    val io: CoroutineDispatcher,
+    val computation: CoroutineDispatcher,
+    val main: CoroutineDispatcher
+)
