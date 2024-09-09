10ea93944087392eadfabe37a774be9bd82d94c8
Lucas Yago <lucasyago63@gmail.com>
2024-09-09 11:11:53 -0300

    cria teste para Characters viewModel e implementa modulo de testes

---
+++ app/src/test/java/com/example/marvelapp/presentation/characters/CharactersViewModelTest.kt
@@ -0,0 +1,79 @@
+package com.example.marvelapp.presentation.characters
+
+import androidx.paging.PagingData
+import com.example.core.domain.model.Character
+import com.example.core.usecase.GetCharactersUseCase
+import com.example.testing.MainCoroutineRule
+import com.example.testing.model.CharacterFactory
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class CharactersViewModelTest{
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Mock
+    lateinit var getCharactersUseCase: GetCharactersUseCase
+
+    private lateinit var charactersViewModel: CharactersViewModel
+
+    private var characterFactory = CharacterFactory()
+
+    private val pagingDataCharacters = PagingData.from(
+        listOf(
+            characterFactory.create(CharacterFactory.Hero.ThreeDMan),
+            characterFactory.create(CharacterFactory.Hero.ABomb)
+        ))
+
+    @ExperimentalCoroutinesApi
+    @Before
+    fun setUp() {
+        charactersViewModel = CharactersViewModel(getCharactersUseCase)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should validate the paging data object values when calling charactersPagingData`() =
+        runTest {
+            whenever(
+                getCharactersUseCase.invoke(any())
+            ).thenReturn(
+                flowOf(pagingDataCharacters)
+            )
+
+            val result = charactersViewModel.getCharactersPagingData("")
+            assertEquals(1, result.count())
+        }
+
+    @ExperimentalCoroutinesApi
+    @Test(expected = RuntimeException::class)
+    fun `should throw an exception when the calling to the use case returns an exception`() =
+        runTest {
+            whenever(getCharactersUseCase.invoke(any()))
+                .thenThrow(RuntimeException())
+
+            charactersViewModel.getCharactersPagingData("")
+        }
+
+}
